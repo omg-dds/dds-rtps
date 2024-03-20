347a6ca044a85d0879edeb51a41fd708ac758369
Ørnulf Staff <ornulf.staff@kongsberg.com>
2024-03-20 15:24:21 +0100

    Add Kongsberg CLA and support for building with InterCOM DDS

---
+++ srcCxx/shape_configurator_intercom_dds.h
@@ -0,0 +1,63 @@
+#include "shape.h"
+#include <InterCOM/ccpp_dds_dcps.h>
+#include <InterCOM/dds_curr_dcps.h>
+
+#define StringSeq_push(seq, val) seq.push_back(val)
+#define LISTENER_STATUS_MASK_ALL STATUS_MASK_ALL
+#define REGISTER_TYPE ShapeTypeTypeSupport::get_instance()->register_type
+#define STRING_IN .c_str()
+#define STRING_ASSIGN(a, b) a = b
+#define STRING_FREE static_cast<void>
+
+inline const char *get_qos_policy_name(DDS::QosPolicyId_t policy_id) {
+  switch (policy_id) {
+  case DDS::USERDATA_QOS_POLICY_ID:
+    return "USERDATA";
+  case DDS::DURABILITY_QOS_POLICY_ID:
+    return "DURABILITY";
+  case DDS::PRESENTATION_QOS_POLICY_ID:
+    return "PRESENTATION";
+  case DDS::DEADLINE_QOS_POLICY_ID:
+    return "DEADLINE";
+  case DDS::LATENCYBUDGET_QOS_POLICY_ID:
+    return "LATENCYBUDGET";
+  case DDS::OWNERSHIP_QOS_POLICY_ID:
+    return "OWNERSHIP";
+  case DDS::OWNERSHIPSTRENGTH_QOS_POLICY_ID:
+    return "OWNERSHIPSTRENGTH";
+  case DDS::LIVELINESS_QOS_POLICY_ID:
+    return "LIVELINESS";
+  case DDS::TIMEBASEDFILTER_QOS_POLICY_ID:
+    return "TIMEBASEDFILTER";
+  case DDS::PARTITION_QOS_POLICY_ID:
+    return "PARTITION";
+  case DDS::RELIABILITY_QOS_POLICY_ID:
+    return "RELIABILITY";
+  case DDS::DESTINATIONORDER_QOS_POLICY_ID:
+    return "DESTINATIONORDER";
+  case DDS::HISTORY_QOS_POLICY_ID:
+    return "HISTORY";
+  case DDS::RESOURCELIMITS_QOS_POLICY_ID:
+    return "RESOURCELIMITS";
+  case DDS::ENTITYFACTORY_QOS_POLICY_ID:
+    return "ENTITYFACTORY";
+  case DDS::WRITERDATALIFECYCLE_QOS_POLICY_ID:
+    return "WRITERDATALIFECYCLE";
+  case DDS::READERDATALIFECYCLE_QOS_POLICY_ID:
+    return "READERDATALIFECYCLE";
+  case DDS::TOPICDATA_QOS_POLICY_ID:
+    return "TOPICDATA";
+  case DDS::GROUPDATA_QOS_POLICY_ID:
+    return "GROUPDATA";
+  case DDS::TRANSPORTPRIORITY_QOS_POLICY_ID:
+    return "TRANSPORTPRIORITY";
+  case DDS::LIFESPAN_QOS_POLICY_ID:
+    return "LIFESPAN";
+  case DDS::DURABILITYSERVICE_QOS_POLICY_ID:
+    return "DURABILITYSERVICE";
+  case DDS::DATA_REPRESENTATION_QOS_POLICY_ID:
+    return "DATAREPRESENTATION";
+  default:
+    return "Unknown";
+  }
+}
