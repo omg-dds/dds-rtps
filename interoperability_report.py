1ad35c172118e85dbd82a83f1cf0b60c6b85cb71
Ørnulf Staff <ornulf.staff@kongsberg.com>
2024-03-20 15:24:21 +0100

    Sleep between publisher and subscriber startup to improve stability

--- interoperability_report.py
+++ interoperability_report.py
@@ -447,10 +447,10 @@ def run_test(
             entity_type.append(f'Publisher_{publisher_number}')
 
         elif('-S ' in parameters[i] or parameters[i].endswith('-S')):
-            # Wait 1 second before running the subscriber if durability is
-            # provided
-            if '-D ' in parameters[i]:
-                time.sleep(1)
+            # Wait 1 second before running the subscriber to avoid conflicts between
+            # the programs on startup
+            time.sleep(1)
+
             entity_process.append(multiprocessing.Process(
                     target=run_subscriber_shape_main,
                     kwargs={
